3b6a86c1e3eb149575cb56e2c7b5cadcdc740093
mmuchai <muchaimercyw@gmail.com>
2017-03-07 17:34:58 +0300

    My scenario redefined

--- OOP_lab.py
+++ OOP_lab.py
@@ -1,3 +1,6 @@
+'''A dress is a sub class of the parent class clothing. 
+It inherits the attributes defined in the super class.
+'''
 class Clothing (object):
   def __init__ (self, color, size, make):
     self.color = color
@@ -7,20 +10,20 @@ class Dress (Clothing):
   def __init__ (self):
     super (Dress, self).__init__()
   def buy_dress(self, make):
-    if make == 'new':
+    if self.make == 'new':
       return "Price is 1000"
-    elif make == 'second_hand':
+    elif self.make == 'second_hand':
       return "Price is 700"
     else:
       return "We do not have this make"
   def sizes_available(self, size):
-    if size < 24:
+    if self.size < 24:
       return "We have your size"
     else:
       return "Size not available"
   def dress_color (self, color):
     colors_available = [red, black, white, blue, beige]
-    if color not in colors_available:
+    if self.color not in colors_available:
       return "Invalid color"
     else:
       return "Color is available"
