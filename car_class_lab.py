c600affacd3810c0746b933387bcda1cdc1ae9ab
mmuchai <muchaimercyw@gmail.com>
2017-03-08 12:35:50 +0300

    Probable solution

--- car_class_lab.py
+++ car_class_lab.py
@@ -6,23 +6,33 @@ class Car(object):
         self.name=str(name)
         self.num_of_doors = []
         self.num_of_wheels = []
+        self.speed = [0]
+        self.drive = int(drive)
 
     def car_doors (self):
-        for door in num_of_doors
-            if (self.name=='Porshe' or self.name=='Koenigsegg'):
+        for door in self.num_of_doors:
+            if (self.name== Porshe or self.name== Koenigsegg):
                 self.num_of_doors.append(2)
                 return self.num_of_doors
             else:
                 self.num_of_doors.append(4)
                 return self.num_of_doors
     def car_wheels (self):
+        for wheel in self.num_of_wheels:
+            if self.car_type=='trailer':
+                self.num_of_wheels.append(8)
+                return self.num_of_wheels
+            else:
+                self.num_of_wheels.append(4)
+                return self.num_of_wheels
 
-        if self.car_type=='trailer':
-            num_of_wheels.append(8)
+    def car_speed(self):
+        if self.car_type == 'trailer':
+            self.speed.append ((11 * self.drive))
         else:
-            num_of_wheels.append(4)
-    def car_speed(self, speed = 0):
-        self.speed = int (speed)
+            self.speed.append ((10***self.drive))
+
+        if self.speed
 toyota = Car('Corolla')
-print (toyota.car_doors(), porshe.car_doors())
+print (toyota.car_doors(), Porshe.num_of_doors)
 print (vars(toyota))
