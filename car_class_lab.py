77d924df249063fe08269cb16c2688e4617671fb
mmuchai <muchaimercyw@gmail.com>
2017-03-08 12:01:45 +0300

    List of cars

--- car_class_lab.py
+++ car_class_lab.py
@@ -1,20 +1,29 @@
 class Car(object):
-    num_of_doors = []
-    num_of_wheels = []
+
     def __init__(self,car_type,model='GM',name = 'General'):
         self.car_type = str (car_type)
         self.model=str(model)
         self.name=str(name)
+        self.num_of_doors = []
+        self.num_of_wheels = []
 
     def car_doors (self):
+
         if (self.name=='Porshe' or self.name=='Koenigsegg'):
-            num_of_doors.append(2)
+
+            self.num_of_doors.append(2)
+            return self.num_of_doors
         else:
-            num_of_doors.append(4)
+            self.num_of_doors.append(4)
+            return self.num_of_doors
     def car_wheels (self):
+
         if self.car_type=='trailer':
             num_of_wheels.append(8)
         else:
             num_of_wheels.append(4)
-    another = car_doors('trailer')
-    print (another)
+    def car_speed(self, speed = 0):
+        self.speed = int (speed)
+toyota = Car('Corolla')
+print (toyota.car_doors(), porshe.car_doors())
+print (vars(toyota))
